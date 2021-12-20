7d6640137b6abb8701cb861fd51ee3fe46b7d21a
nasimul hasan <nasimulhasandeep@gmail.com>
2021-12-20 19:36:17 +0600

    authentication

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Learning Laravel</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+</head>
+
+<body class="min-h-screen bg-gradient-to-tr from-green-400 to-green-500 flex items-center justify-center">
+    <h1 class="text-6xl font-semibold text-center text-white underline decoration-blue-800 decoration-wavy">This is a laravel learning website </h1>
+</body>
+
+</html>
